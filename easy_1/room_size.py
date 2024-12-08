5e21e3c8ce413e48ce94712cc598ca10fce6c8c2
Joel Turske <joelturske@gmail.com>
2024-12-07 23:44:05 -0500

    Further exploration, added user input for type of measurement. Didn't account for errors.

--- easy_1/room_size.py
+++ easy_1/room_size.py
@@ -1,17 +1,18 @@
-# Get user input for length and width of room
-print(
-    """Give me the length and width of your room in meters
-    and I'll give you the area in square meters
-    and square feet.""")
-length = int(input("How many meters long is your room? "))
-width = int(input("How many meters wide is your room? "))
+# Get user input for feet or meters and then length and width of room.
+print("I'll give you the area of your room.")
+unit = input("What unit of measurement? Enter f for feet or m for meters: ")
+length = float(input("How long is your room? "))
+width = float(input("How wide is your room? "))
 
-# Calculate area in meters and feet
-sq_meters = length * width
-sq_feet = sq_meters * 10.7639
-
-# Print area in sq meters and sq feet
-print(f"""Your room's area is {sq_meters:.2f} square meters 
-      and {sq_feet:.2f} square feet""")
+if unit == 'm':
+    # Calculate area in meters and feet, print.
+    sq_meters = length * width
+    sq_feet = sq_meters * 10.7639
+    print(f"Your room's area is {sq_meters:.2f}({sq_feet:.2f})")
+elif unit == 'f':
+    # Calculate area in feet and meters, print.
+    sq_feet = length * width
+    sq_meters = sq_feet / 10.7639
+    print(f"Your room's area is {sq_feet:.2f} ({sq_meters:.2f})")
 
 
