52fa8ee19f4aeb2814c2b51d5fe6ca3bcf999cde
Joel Turske <joelturske@gmail.com>
2024-12-07 22:57:53 -0500

    added further exploration.

--- easy_1/odd_nums.py
+++ easy_1/odd_nums.py
@@ -1,2 +1,7 @@
-for num in range(1, 101, 2):
+# Further exploration add user interaction
+print("Hi, I print odd numbers.")
+start = int(input("What number should I start at? "))
+stop = int(input("What number should I stop at? "))
+
+for num in range(start, stop + 1, 2):
     print(num)
