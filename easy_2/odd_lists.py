c14487322b9c9161f0c69af639466918149954ae
Joel Turske <joelturske@gmail.com>
2025-01-04 09:52:54 -0500

    Pull out the items from even numbered indexes of a list with list comprehension and slicing.

---
+++ easy_2/odd_lists.py
@@ -0,0 +1,20 @@
+# First solution that came to mind
+def oddities(lst):
+    return [item for item in lst if lst.index(item) % 2 == 0]
+
+print(oddities([2, 3, 4, 5, 6]) == [2, 4, 6])  # True
+print(oddities([1, 2, 3, 4]) == [1, 3])        # True
+print(oddities(["abc", "def"]) == ['abc'])     # True
+print(oddities([123]) == [123])                # True
+print(oddities([]) == [])                      # True
+
+print()
+# Bonus solution with slicing
+def oddities(lst):
+    return lst[::2]
+
+print(oddities([2, 3, 4, 5, 6]) == [2, 4, 6])  # True
+print(oddities([1, 2, 3, 4]) == [1, 3])        # True
+print(oddities(["abc", "def"]) == ['abc'])     # True
+print(oddities([123]) == [123])                # True
+print(oddities([]) == [])                      # True
