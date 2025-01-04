011071ff63116ed620440fde159d124e3540e121
Joel Turske <joelturske@gmail.com>
2025-01-04 11:09:42 -0500

    Did further exploration of random age exercise.

--- easy_2/teddy_age.py
+++ easy_2/teddy_age.py
@@ -1,4 +1,18 @@
 import random
 
-age = random.randint(20, 100)
-print(f"Teddy is {age} years old!")
+def prompt(message):
+    print(f'==> {message}')
+
+def output_person_age(age_arg, name):
+    prompt(f'{name} is {age_arg} years old!')
+
+def main():
+    prompt("What is your name?")
+    name = input()
+    if not name:
+        name = 'Teddy'
+    age = random.randint(20, 100)
+    
+    output_person_age(age, name)
+
+main()
