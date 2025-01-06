47e338478be3805c4b1758af5f72ef17e4e1d432
Joel Turske <joelturske@gmail.com>
2025-01-05 21:41:03 -0500

    Moving easy_3 problems into their own folder.

--- easy_2/string_string.py
+++
@@ -1,9 +0,0 @@
-def stringy(length):
-    result = ['1' if i % 2 == 0 else '0' for i in range (length)]
-    return ''.join(result)
-    
-
-print(stringy(6) == "101010")           # True
-print(stringy(9) == "101010101")        # True
-print(stringy(4) == "1010")             # True
-print(stringy(7) == "1010101")          # True
